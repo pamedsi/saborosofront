4fc158f3c79529335f38d3f65ae885a0d97af6d1
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-08 23:44:36 -0300

    cria componente de quantidade de quentinhas

---
+++ src/app/Components/container/container.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+export enum FieldType {
+  Text = 'text',
+  Checkbox = 'checkbox',
+  Radio = 'radio',
+  Select = 'select',
+  TextArea = 'textarea',
+}
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css']
+})
+export class ContainerComponent {
+  @Input() content: string
+  @Input() selectedField?: FieldType;
+  protected readonly FieldType = FieldType
+  @Input() numberOfOptions?: number;
+
+  public generateNumberOptions(numberOptions: number): number[] {
+    return Array.from({ length: numberOptions + 1 }, (_, i) => i);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FieldType} from "./Components/container/container.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'saborosofront';
+  protected readonly FieldType = FieldType;
 }
