24649978ba22c14bbe69a61cae02c53005e981cd
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-09 00:53:19 -0300

    cria componente de menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './Components/container/container.component';
 import { MenuItemComponent } from './Components/menu-item/menu-item.component';
 import {NgOptimizedImage} from "@angular/common";
+import { MenuComponent } from './Components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     MenuItemComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
